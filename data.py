2a41ffe7c61d978efb0df673412446ad55708418
DilshodM <dilshodmurtazoyev5@gmail.com>
2022-09-01 22:26:47 +0500

    python 2-kun

--- data.py
+++ data.py
@@ -192,3 +192,49 @@ def play(x=10):
         yana = int(input("Yana o'ynaymizmi? Ha(1)/Yo'q(0):"))
             
 play()
+
+class User:
+    def __int__(self,name,username,email):
+        self.name = name
+        self.uname = username
+        self.mail = email
+    
+    def describe():
+        pass
+    
+    def get_email():
+        pass
+
+
+
+
+
+
+
+
+    class Talaba:
+    """Talaba nomli klass yaratamiz"""
+    def __init__(self,ism,familiya,tyil):
+        """Talabaning xususiyatlari"""
+        self.ism = ism
+        self.familiya = familiya
+        self.tyil = tyil
+    
+    def get_name(self):
+        """Talabaning ismini qaytaradi"""
+        return self.ism
+    
+    def get_lastname(self):
+        """Talabaning familiyasini qaytaradi"""
+        return self.familiya
+    
+    def get_fullname(self):
+        """Talabaning ism-familiyasini qaytaradi"""
+        return f"{self.ism} {self.familiya}"
+    
+    def get_age(self,yil):
+        """Talabaning yoshini qaytaradi"""
+        return yil-self.tyil
+    
+    def tanishtir(self):
+        print(f"Ismim {self.ism} {self.familiya}. {self.tyil} yilda tu'gilganman")
